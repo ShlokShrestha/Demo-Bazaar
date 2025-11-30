616085930658e3045632c4464f3078ef317c3526
shlokshrestha <shlokstha99@gmail.com>
2025-11-30 20:27:43 +0545

    change in zustand state

--- components/Navbar/Cart.tsx
+++ components/Navbar/Cart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useProductStore } from "@/store/store";
+import { useProductStore } from "@/Store/store";
 import { useState, useEffect, useRef } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
