04b700bfd3bf2282e6a8e2c524201df30bb35e0b
shlokshrestha <shlokstha99@gmail.com>
2026-01-18 19:14:29 +0545

    fix: change in payment status naming and added other status

--- lib/paymentStatus.ts
+++ lib/paymentStatus.ts
@@ -1,14 +1,17 @@
 import { PurchaseStatus } from "@/app/generated/prisma/enums";
 
-export function mapKhaltiStatusToEnum(khaltiStatus: string): PurchaseStatus {
+export function paymentStatus(khaltiStatus: string): PurchaseStatus {
   switch (khaltiStatus) {
     case "Completed":
+    case "COMPLETE":
     case "success":
       return PurchaseStatus.COMPLETED;
     case "Failed":
     case "failed":
+    case "CANCELED":
       return PurchaseStatus.FAILED;
     case "Pending":
+    case "PENDING":
     case "pending":
       return PurchaseStatus.PENDING;
     case "Expired":
